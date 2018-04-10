9b0c07e65b88c07ca066f0639da42214544d3477
ardcsr <ard_csr@hotmail.com>
2018-04-10 10:37:55 +0700

    new edit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  editInput=false;
   constructor() { }
 
   ngOnInit() {
+  }
+  onStatusEdit(){
+    switch (this.editInput) {
+      case true:
+        this.editInput=false;
+        break;
+    
+      default:
+      this.editInput=true;
+        break;
+    }
   }
 
 }
