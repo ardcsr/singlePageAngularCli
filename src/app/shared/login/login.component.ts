d4963032d1721255f2566fc800deffdcb4a29a45
ardcsr <ard_csr@hotmail.com>
2018-06-03 21:52:18 +0700

    new update

--- src/app/shared/login/login.component.ts
+++ src/app/shared/login/login.component.ts
@@ -8,18 +8,33 @@ import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms'
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  signInGroup: FormGroup; 
-  constructor(private api: PageService,private formBuilder: FormBuilder) { }
+  signInGroup: FormGroup;
+  constructor(private api: PageService, private formBuilder: FormBuilder) { }
   hide = true;
 
   ngOnInit() {
-  this.buildForm();
+    this.buildForm();
   }
   buildForm() {
     this.signInGroup = this.formBuilder.group({
       username: this.formBuilder.control(null, [Validators.required]),
       password: this.formBuilder.control(null, [Validators.required]),
     })
+  }
+  signIn() {
+    this.api.signIn(this.signInGroup.value).subscribe(
+      res => {
+        if (res.statusCode == 200) {
+          this.api.createToken(res.data);
+          var userInfo = JSON.parse(decodeURIComponent(atob(res.data.split('.')[1])));
+          localStorage.setItem('userInfo', userInfo)
+          // this.router.navigate(["/edit"])
+        }
+      },
+      error => {
+        console.log(error)
+      }
+    )
   }
 
 }
